88b24f7336dd33ad5b91a208e4520b1524694596
johnwargo <john@johnwargo.com>
2025-02-16 08:13:34 -0500

    Update esp32-request-repeater.ino
    it works!

--- esp32-request-repeater.ino
+++ esp32-request-repeater.ino
@@ -23,6 +23,10 @@ const char* ssid = WIFI_SSID;
 const char* password = WIFI_PASSWORD;
 const long minutes2Microseconds = 60000000;
 
+// Create a restart counter to track how many times the sketch restarts,
+// store in the RTC (realtime clock) storage area
+RTC_DATA_ATTR unsigned int restartCounter = 0;
+
 HTTPClient http;
 
 void setup() {
@@ -34,6 +38,13 @@ void setup() {
   Serial.println("* ESP32 Request Repeater *");
   Serial.println("**************************");
 
+  restartCounter += 1;
+  if (restartCounter < 2) {
+    Serial.println("Sketch started once\n");
+  } else {
+    Serial.printf("Sketch restarted %d times\n", restartCounter);
+  }
+
   // Check to make sure we have Wi-Fi credentials
   // before trying to use them
   if (String(ssid).isEmpty() || String(password).isEmpty()) {
@@ -41,7 +52,12 @@ void setup() {
     for (;;) {}
   }
 
-  print_wakeup_reason();
+  // Wait 30 seconds to provide some time to deploy updates to the sketch otherwise it will
+  // disconnect as soon as its done and you won't be able to save updates to the device
+  Serial.println("Waiting 30 seconds to allow for sketch uploads");
+  delay(30000);
+
+  displayWakupReason();
   esp_sleep_enable_timer_wakeup(SLEEP_DURATION_MINUTES * minutes2Microseconds);
   if (connectToNetwork()) callRemoteHost();
   esp_deep_sleep_start();
@@ -102,17 +118,31 @@ void callRemoteHost() {
   http.end();
 }
 
-// From https://randomnerdtutorials.com/esp32-deep-sleep-arduino-ide-wake-up-sources/
-void print_wakeup_reason() {
+
+// docs: https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/sleep_modes.html#_CPPv418esp_sleep_source_t
+void displayWakupReason() {
+
   esp_sleep_wakeup_cause_t wakeupReason;
+  String msg;
 
   wakeupReason = esp_sleep_get_wakeup_cause();
   switch (wakeupReason) {
-    case ESP_SLEEP_WAKEUP_EXT0: Serial.println("Wakeup caused by external signal using RTC_IO"); break;
-    case ESP_SLEEP_WAKEUP_EXT1: Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
-    case ESP_SLEEP_WAKEUP_TIMER: Serial.println("Wakeup caused by timer"); break;
-    case ESP_SLEEP_WAKEUP_TOUCHPAD: Serial.println("Wakeup caused by touchpad"); break;
-    case ESP_SLEEP_WAKEUP_ULP: Serial.println("Wakeup caused by ULP program"); break;
-    default: Serial.printf("Wakeup not caused by deep sleep: %d\n", wakeupReason); break;
+    case ESP_SLEEP_WAKEUP_BT: msg = "BT (light sleep only)"; break;  // added for completeness
+    case ESP_SLEEP_WAKEUP_COCPU: msg = "COCPU int"; break;
+    case ESP_SLEEP_WAKEUP_COCPU_TRAP_TRIG: msg = "COCPU crash"; break;
+    case ESP_SLEEP_WAKEUP_EXT0: msg = "External signal using RTC_IO"; break;
+    case ESP_SLEEP_WAKEUP_EXT1: msg = "External signal using RTC_CNTL"; break;  // added for completeness
+    case ESP_SLEEP_WAKEUP_GPIO: msg = "GPIO (light sleep only on ESP32, S2 and S3)"; break;
+    case ESP_SLEEP_WAKEUP_TIMER: msg = "Timer"; break;  // this is the one we care about
+    case ESP_SLEEP_WAKEUP_TOUCHPAD: msg = "Touchpad"; break;
+    case ESP_SLEEP_WAKEUP_UART: msg = "UART (light sleep only)"; break;  // added for completeness
+    case ESP_SLEEP_WAKEUP_ULP: msg = "ULP program"; break;
+    // means sketch started because a new version of the sketch was loaded or powered up not from sleep (fresh start)
+    case ESP_SLEEP_WAKEUP_UNDEFINED: msg = "(Deep Sleep) Not caused by exit from deep sleep."; break;
+    // this one's in the docs, but not in the library apparently
+    // case ESP_SLEEP_WAKEUP_VAD: msg = "Wakeup caused by VAD"; break;
+    case ESP_SLEEP_WAKEUP_WIFI: msg = "WIFI (light sleep only)"; break;  // added for completeness
+    default: msg = "Unknown (" + String(wakeupReason) + ")"; break;
   }
+  Serial.println("Wakeup Reason: " + msg);
 }
